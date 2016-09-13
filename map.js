66f173ebd9f04592efdbff36855837b031c013de
George Thomas <georgetamaothomas@gmail.com>
2016-09-13 22:33:54 +0100

    Add linear interpolation code

--- map.js
+++ map.js
@@ -64,4 +64,46 @@ function fetchCommutes(direction) {
 function fetchActivity(id) {
   return fetch("https://nene.strava.com/flyby/stream_compare/" + id + "/" + id)
     .then(x => x.json())
+}657647143
+
+function posAtTimeT(activity, t) {
+  const i = greatestIndexLessThanT_v1(activity, t);
+  const p1 = JSON.parse(JSON.stringify(activity[i]));
+  const p2 = JSON.parse(JSON.stringify(activity[i+1]));
+  
+  t -= p1.time;
+  p2.time -= p1.time;
+  p1.time -= p1.time;
+  t /= p2.time;
+
+  return {
+    lat: (1-t) * p1.point.lat + t * p2.point.lat;
+    lng: t * p1.point.lng + (1-t) * p2.point.lng;
+  }
+}
+
+function greatestIndexLessThanT_v1(activity, t) {
+  let i = 0;
+
+  while (!(activity[i].time <= t && t <= activity[i+1].time)) {
+    i++;
+  }
+
+  return i;
+}
+
+function greatestIndexLessThanT_v2(activity, t) {
+  let start = 0;
+  let end = activity.length - 1;
+  let i = Math.floor((start + end) / 2);
+
+  while (!(activity[i].time <= t && t <= activity[i+1].time)) {
+    if (activity[i].time <= t) {
+      start = i + 1;
+      i = Math.floor((start + end) / 2);
+    } 
+  }
+
+  return i;
+
 }
