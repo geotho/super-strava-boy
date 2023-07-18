6e51acfcc8d5ea51ad5540de2b042d77033e9323
George Thomas <georgetamayothomas@gmail.com>
2023-07-18 23:41:15 +0100

    Add local activity pulling

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 const pubStravaToken = "57a2e0bbc8a2789e2a56cb2f911d76d6ce48b5e5";
-const cross = "https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_close_black_16px.svg"
+const cross =
+  "https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_close_black_16px.svg";
 
 let map;
 let playInterval;
@@ -9,8 +10,8 @@ const streams = [];
 const markers = [];
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.508235, lng: -0.324592},
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 51.508235, lng: -0.324592 },
     zoom: 16,
     styles: [
       // {
@@ -24,37 +25,46 @@ function initMap() {
   });
 
   fetchCommutes("to_work")
-    .then(x => x.map(j => j.id))
-    .then(ids => {
+    .then((x) => x.map((j) => j.id))
+    .then((ids) => {
       const proms = [];
       for (const id of ids) {
-        proms.push(fetchActivity(id).then(x => {
-          if (x.stream[0].point.lng < -0.321546) {
-            return;
-          }
-          map.data.addGeoJson(makeGeoJson(x.stream));
-          return x;
-        }))
+        proms.push(
+          fetchActivityLocal(id).then((x) => {
+            if (x.stream[0].point.lng < -0.321546) {
+              return;
+            }
+            map.data.addGeoJson(makeGeoJson(x.stream));
+            return x;
+          })
+        );
       }
       return Promise.all(proms);
     })
-    .then(activities => {
-      const minTime = activities.filter(Boolean).map(a => a.stream[0].time).sort()[0];
+    .then((activities) => {
+      const minTime = activities
+        .filter(Boolean)
+        .map((a) => a.stream[0].time)
+        .sort()[0];
       console.log("minTime = ", minTime);
       for (const a of activities.filter(Boolean)) {
         relativeTime(a);
         streams.push(a.stream);
-        markers.push(new google.maps.Marker({
-          position: a.stream[0].point,
-          map: map,
-        }));
+        markers.push(
+          new google.maps.Marker({
+            position: a.stream[0].point,
+            map: map,
+          })
+        );
       }
 
       console.log("markers = ", markers);
       console.log("streams = ", streams);
 
-      console.log(streams.filter(Boolean).map(s => posAtTimeT(s, 1447666275)))
-    })
+      console.log(
+        streams.filter(Boolean).map((s) => posAtTimeT(s, 1447666275))
+      );
+    });
 }
 
 function moveMarkersToTimeT(t) {
@@ -71,29 +81,36 @@ function makeGeoJson(stream) {
     },
     geometry: {
       type: "LineString",
-      coordinates: stream.map(s => [s.point.lng, s.point.lat]),
-    }
+      coordinates: stream.map((s) => [s.point.lng, s.point.lat]),
+    },
   };
 }
 
-
 function fetchCommutes(direction) {
-  const p = fetch("activities200.json")
-    .then(x => x.json())
+  const p = fetch("activities200.json").then((x) => x.json());
 
   if (direction === "to_work") {
     // get the morning commutes
-    return p.then(acts => acts.filter(a => new Date(a.start_date).getHours() < 12))
+    return p.then((acts) =>
+      acts.filter((a) => new Date(a.start_date).getHours() < 12)
+    );
   } else if (direction === "from_work") {
     // get the evening commutes
-    return p.then(acts => acts.filter(a => new Date(a.start_date).getHours() >= 12))
+    return p.then((acts) =>
+      acts.filter((a) => new Date(a.start_date).getHours() >= 12)
+    );
   }
-  return p
+  return p;
 }
 
 function fetchActivity(id) {
-  return fetch("https://nene.strava.com/flyby/stream_compare/" + id + "/" + id)
-    .then(x => x.json())
+  return fetch(
+    "https://nene.strava.com/flyby/stream_compare/" + id + "/" + id
+  ).then((x) => x.json());
+}
+
+function fetchActivityLocal(id) {
+  return fetch("scripts/pulldata/out/" + id + ".json").then((x) => x.json());
 }
 
 function posAtTimeT(activity, t) {
@@ -103,7 +120,7 @@ function posAtTimeT(activity, t) {
     activity.push(activity[i]);
   }
 
-  const p2 = JSON.parse(JSON.stringify(activity[i+1]));
+  const p2 = JSON.parse(JSON.stringify(activity[i + 1]));
 
   t -= p1.time;
   if (p1.time === p2.time) {
@@ -114,15 +131,18 @@ function posAtTimeT(activity, t) {
   t /= p2.time;
 
   return {
-    lat: (1-t) * p1.point.lat + t * p2.point.lat,
-    lng: (1-t) * p1.point.lng + t * p2.point.lng,
-  }
+    lat: (1 - t) * p1.point.lat + t * p2.point.lat,
+    lng: (1 - t) * p1.point.lng + t * p2.point.lng,
+  };
 }
 
 function greatestIndexLessThanT_v1(activity, t) {
   let i = 0;
 
-  while (i < activity.length - 1 && !(activity[i].time <= t && t <= activity[i+1].time)) {
+  while (
+    i < activity.length - 1 &&
+    !(activity[i].time <= t && t <= activity[i + 1].time)
+  ) {
     i++;
   }
 
@@ -134,7 +154,7 @@ function greatestIndexLessThanT_v2(activity, t) {
   let end = activity.length - 1;
   let i = Math.floor((start + end) / 2);
 
-  while (!(activity[i].time <= t && t <= activity[i+1].time)) {
+  while (!(activity[i].time <= t && t <= activity[i + 1].time)) {
     if (activity[i].time <= t) {
       start = i + 1;
       i = Math.floor((start + end) / 2);
@@ -142,7 +162,6 @@ function greatestIndexLessThanT_v2(activity, t) {
   }
 
   return i;
-
 }
 
 function relativeTime(activity) {
@@ -164,6 +183,6 @@ function playPause(btn) {
     moveMarkersToTimeT(markerTime);
     markerTime += 1;
     document.getElementById("time").value = markerTime;
-  }, 10)
+  }, 10);
   btn.innerHTML = "Pause";
 }
